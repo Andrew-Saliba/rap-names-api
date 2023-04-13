5af2c98163483ef0d09a4cd94da1cc6f8471b27b
Andrew Saliba <andrew-saliba@hotmail.com>
2023-04-13 15:25:29 -0500

    Added Drake, J Cole, Kendrick Lamar

--- server.js
+++ server.js
@@ -4,17 +4,31 @@ const PORT = 8000
 
 const rapperNames = {
     '21 savage':{
-        'age':29,
+        'dateOfBirth': '1992-10-22',
       'birthName':'Sheyaa Bin Abraham-Joseph',
       'birthLocation':'London, UK',
     },
     'chance the rapper':{
-        'age':29,
+        'dateOfBirth': '1993-04-16',
       'birthName':'Chancellor Bennett',
       'birthLocation':'Chicago, Illinois',
     },
+    'Drake':{
+        'dateOfBirth': '1986-10-24',
+        'birthName':'Aubrey Drake Graham',
+        'birthLocation':'Toronto, Ontario, Canada',
+    },
+    'Kendrick Lamar':{
+        'dateOfBirth': '1987-06-17',
+        'birthName':'Kendrick Lamar Duckworth',
+        'birthLocation':'Compton, California',
+    },
+    'J. Cole':{
+        'dateOfBirth': '1985-01-28',
+        'birthName':'Jermaine Lamar Cole',
+        'birthLocation':'Frankfurt, Germany',
     'unknown':{
-        'age':100,
+        'dateOfBirth': '1923-01-01',
         'birthName':'unknown',
         'birthLocation':'unknown',
         },
